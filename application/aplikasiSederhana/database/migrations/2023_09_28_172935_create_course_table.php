ce97c8f8f7b3825da15fe099ef08141bb05ae3d0
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2023-09-29 00:32:43 +0700

    CREATE course-table

---
+++ application/aplikasiSederhana/database/migrations/2023_09_28_172935_create_course_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    //Run the migrations
+    public function up(): void
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->id(); //Primary key
+            $table->unsignedBigInteger('idKategori'); //Foreign key
+            $table->string('kodeCourse');
+            $table->string('namaCourse');
+            $table->timestamps();
+
+            //Define foreign key to "Kategori" table
+            $table->foreign('idKategori')->references('id')->on('kategori');
+        });
+    }
+
+    //Reverse the migrations
+    public function down(): void
+    {
+        Schema::dropIfExists('course');
+    }
+};
