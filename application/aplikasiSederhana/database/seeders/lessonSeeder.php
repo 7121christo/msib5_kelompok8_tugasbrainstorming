2b090fa22d523451ddd3e180820255018990d1e6
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2023-09-29 16:05:15 +0700

    feat:::

--- application/aplikasiSederhana/database/seeders/lessonSeeder.php
+++ application/aplikasiSederhana/database/seeders/lessonSeeder.php
@@ -17,24 +17,28 @@ class lessonSeeder extends Seeder
         $Lesson->course_id = '1';
         $Lesson->judul = 'VARIABLE';
         $Lesson->deskripsi = 'Mempelajari variable di PHP';
+        $Lesson->materi='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eaque, magni, vitae dicta tempora repellendus soluta ut exercitationem nostrum, accusantium cumque harum ducimus et. Magni quasi corporis aliquid voluptatibus, veniam velit aut, est voluptates maiores eius neque porro voluptatem fuga? Cupiditate, necessitatibus sit. Saepe, amet. Quaerat architecto quo ipsa totam voluptate. Iusto exercitationem a, repellat numquam doloremque ipsa quae, aut repellendus deserunt voluptatibus perferendis nobis sapiente enim. Rem, nulla sit. Tempore fuga corrupti odio! Error dolor adipisci laudantium, aperiam, eligendi aliquid consequatur necessitatibus neque deleniti sunt atque architecto deserunt saepe id, ipsum beatae. Tempore blanditiis officiis quo nihil corrupti excepturi!';
         $Lesson->save();
 
         $Lesson = new Lesson();
         $Lesson->course_id = '2';
         $Lesson->judul = 'ELEMENT';
         $Lesson->deskripsi = 'Mempelajari element di HTML';
+        $Lesson->materi='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eaque, magni, vitae dicta tempora repellendus soluta ut exercitationem nostrum, accusantium cumque harum ducimus et. Magni quasi corporis aliquid voluptatibus, veniam velit aut, est voluptates maiores eius neque porro voluptatem fuga? Cupiditate, necessitatibus sit. Saepe, amet. Quaerat architecto quo ipsa totam voluptate. Iusto exercitationem a, repellat numquam doloremque ipsa quae, aut repellendus deserunt voluptatibus perferendis nobis sapiente enim. Rem, nulla sit. Tempore fuga corrupti odio! Error dolor adipisci laudantium, aperiam, eligendi aliquid consequatur necessitatibus neque deleniti sunt atque architecto deserunt saepe id, ipsum beatae. Tempore blanditiis officiis quo nihil corrupti excepturi!';
         $Lesson->save();
 
         $Lesson = new Lesson();
         $Lesson->course_id = '3';
         $Lesson->judul = 'ARITHMETIC';
         $Lesson->deskripsi = 'Mempelajari operator arithmetic di JS';
+        $Lesson->materi='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eaque, magni, vitae dicta tempora repellendus soluta ut exercitationem nostrum, accusantium cumque harum ducimus et. Magni quasi corporis aliquid voluptatibus, veniam velit aut, est voluptates maiores eius neque porro voluptatem fuga? Cupiditate, necessitatibus sit. Saepe, amet. Quaerat architecto quo ipsa totam voluptate. Iusto exercitationem a, repellat numquam doloremque ipsa quae, aut repellendus deserunt voluptatibus perferendis nobis sapiente enim. Rem, nulla sit. Tempore fuga corrupti odio! Error dolor adipisci laudantium, aperiam, eligendi aliquid consequatur necessitatibus neque deleniti sunt atque architecto deserunt saepe id, ipsum beatae. Tempore blanditiis officiis quo nihil corrupti excepturi!';
         $Lesson->save();
 
         $Lesson = new Lesson();
         $Lesson->course_id = '4';
         $Lesson->judul = 'SELECTOR';
         $Lesson->deskripsi = 'Mempelajari operator selector di CSS';
+        $Lesson->materi='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eaque, magni, vitae dicta tempora repellendus soluta ut exercitationem nostrum, accusantium cumque harum ducimus et. Magni quasi corporis aliquid voluptatibus, veniam velit aut, est voluptates maiores eius neque porro voluptatem fuga? Cupiditate, necessitatibus sit. Saepe, amet. Quaerat architecto quo ipsa totam voluptate. Iusto exercitationem a, repellat numquam doloremque ipsa quae, aut repellendus deserunt voluptatibus perferendis nobis sapiente enim. Rem, nulla sit. Tempore fuga corrupti odio! Error dolor adipisci laudantium, aperiam, eligendi aliquid consequatur necessitatibus neque deleniti sunt atque architecto deserunt saepe id, ipsum beatae. Tempore blanditiis officiis quo nihil corrupti excepturi!';
         $Lesson->save();
     }
 }
---
+++ application/aplikasiSederhana/database/seeders/userSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'username' => 'user1',
+            'nama' => 'ininamaku',
+            'password' => 'sjdhfgkujh'
+        ]);
+    }
+}
